7ae46265ddd3e66925e2abec33311aa3f461551a
Alex Lee <aklee2106@gmail.com>
2020-11-10 20:37:49 -0500

    convert authClass from class component to react hook

--- src/components/AuthClass.jsx
+++ src/components/AuthClass.jsx
@@ -2,29 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../actions/demo_actions';
 
-class AuthClass extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
-
-    componentDidUpdate() {
-        document.title = this.state.username;
-    }
-
+const AuthHook = ({login}) =>{
+    
     handleInput = e => {
         e.preventDefault()
-        this.setState({[e.target.name]: e.target.value})
     }
 
-    handleSubmit = () => this.props.login(this.state)
+    handleSubmit = () => login()
 
-    handleDemo = () => this.props.login({username: 'User', password: '123456'})
+    handleDemo = () => login({})
 
-    render() {
+   
         return (
             <div id="auth" className="column"> 
               <div className="auth module">
@@ -34,25 +22,25 @@ class AuthClass extends React.Component {
                     name="username"
                     type="text"
                     placeholder="Username"
-                    value={this.username}
-                    onChange={this.handleInput} 
+                    value={username}
+                    onChange={handleInput} 
                     />
                   <input
                     name="password"
                     type="password" 
                     placeholder="Password"
-                    value={this.password}
-                    onChange={this.handleInput}
+                    value={password}
+                    onChange={handleInput}
                     />
                   <div className="row submit">
-                    <button onClick={this.handleDemo}>Demo User</button>
-                    <button onClick={this.handleSubmit}>Submit &rarr;</button>
+                    <button onClick={handleDemo}>Demo User</button>
+                    <button onClick={handleSubmit}>Submit &rarr;</button>
                   </div>
                 </form>
               </div>
             </div>
         );
-    }
+    
 }
 
 const mdp = dispatch => ({
